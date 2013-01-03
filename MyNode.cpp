78fe66a3fd9c3f16720f1b807a36569b6b9c6664
Dzidorius Martinaitis <kafka@blusa.(none)>
2013-01-03 15:17:06 +0000

    license

--- MyNode.cpp
+++ MyNode.cpp
@@ -2,10 +2,22 @@
 // Name        : MyNode.cpp
 // Author      : Dzidorius Martinaitis 
 // Version     :
-// Copyright   : MIT
+// Copyright   : GPL
 // Description : Simple implementation of a tree
 //============================================================================
+/*
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include<iostream>
 #include<fstream>
 #include <string>
